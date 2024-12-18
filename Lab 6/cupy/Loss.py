48f182bddd326918d07a87f201498410cc3e81c9
iwtba4188 <X69599596@gmail.com>
2024-12-18 13:24:09 +0800

    feat: advance 0.9457

---
+++ Lab 6/cupy/Loss.py
@@ -0,0 +1,37 @@
+import cupy as cp
+import numpy as np
+
+
+def compute_CCE_loss(AL, Y):
+    """
+    Implement the categorical cross-entropy loss function using the above formula.
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (n, C)
+    Y -- true "label" vector (one hot vector, for example: [1,0,0] represents rock, [0,1,0] represents paper, [0,0,1] represents scissors
+                                      in a Rock-Paper-Scissors, shape: (n, C)
+
+    Returns:
+    loss -- categorical cross-entropy loss
+    """
+
+    n = Y.shape[0]
+
+    # Compute loss from aL and y.
+    ### START CODE HERE ### (≈ 1 line of code)
+    epsilon = 1e-5
+    loss = -cp.sum(Y * cp.log(AL + epsilon)) / n
+    ### END CODE HERE ###
+
+    loss = cp.squeeze(
+        loss
+    )  # To make sure your loss's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert loss.shape == ()
+
+    return loss
+
+
+def compute_MSE_loss(AL, Y):
+    m = Y.shape[0]
+    loss = (1 / m) * cp.sum(cp.square(AL - Y))
+    return loss
